c68135c975877a722dc9103d88b8ac30afc9aa01
everup <everup@163.com>
2023-04-07 18:07:05 +0800

    日志记录中间件

--- api/app/Http/Middleware/AfterMiddleware.php
+++ api/app/Http/Middleware/AfterMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class AfterMiddleware
 {
@@ -17,7 +18,11 @@ class AfterMiddleware
     {
         //主要操作 是处理日志信息 记录请求数据以及 请求头信息
 
-
+        $method = $request->isMethod('POST');
+        if($method){
+            $uid = app('auth')->user()->id??'';
+            Log::info('Request ', ['uid'=>$uid,'url' => $request->decodedPath(),'param' => $request->all()]);
+        }
 
         return $next($request);
     }
